5815de563b107028087b37a1388d8a4e67007d26
krgarima <kumargarima59@gmail.com>
2024-02-15 19:49:28 +0530

    fix

--- context/video-context.tsx
+++ context/video-context.tsx
@@ -3,9 +3,9 @@
 import videoDetails from "../public/data/videoDetails.json";
 import React, { useState, createContext } from "react";
 
-const videoContext = createContext(null);
+const videoContext = createContext<any>(null);
 
-const VideoContextProvider = ({ children }) => {
+const VideoContextProvider = ({ children }: any) => {
   const [videoList, setVideoList] = useState(videoDetails);
   const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
 
