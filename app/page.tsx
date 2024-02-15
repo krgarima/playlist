f5bcab0e059a341f182e5db5b29761f3eb0aa848
krgarima <kumargarima59@gmail.com>
2024-02-15 20:40:32 +0530

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import Navbar from "@/components/Navbar";
+import Playlist from "@/components/Video/Playlist";
+import VideoPlayer from "@/components/Video/VideoPlayer";
 import { VideoContextProvider } from "@/context/video-context";
-import Home from "@/pages/Home";
 
 export default function App() {
   return (
     <VideoContextProvider>
-      <Home />
+      <div className="h-screen w-screen flex flex-col ">
+        <Navbar />
+        <main className="flex w-full flex-col md:flex-row flex-1 pt-16 overflow-hidden px-6">
+          <VideoPlayer />
+          <Playlist />
+        </main>
+      </div>
     </VideoContextProvider>
   );
 }
